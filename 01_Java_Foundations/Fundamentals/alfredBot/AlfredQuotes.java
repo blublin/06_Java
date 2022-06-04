135ce2e0c92170d1d6e1f1e86849054035b28cf3
blublin <lublin.ben@gmail.com>
2022-06-04 11:19:21 -0700

    AlfredBot with ninja and sensei bonus

--- 01_Java_Foundations/Fundamentals/alfredBot/AlfredQuotes.java
+++ 01_Java_Foundations/Fundamentals/alfredBot/AlfredQuotes.java
@@ -1,5 +1,6 @@
 import java.util.Date;
 import java.util.Random;
+import java.text.SimpleDateFormat;  
 
 public class AlfredQuotes {
     
@@ -7,7 +8,45 @@ public class AlfredQuotes {
         // You do not need to code here, this is an example method
         return "Hello, lovely to see you. How are you?";
     }
+
+    // Sensei Version :: no parameters
+    public String guestGreeting() {
+        // Create date formatter to output current hour in 0-23 format
+        SimpleDateFormat sdFormatter = new SimpleDateFormat("H");
+        // Simple Date Format returns string
+        // Convert to primitive int
+        // https://stackoverflow.com/a/5585876/19170401
+        int currentHour = Integer.parseInt(sdFormatter.format(new Date()));
+        
+        // Test validity and type
+        // System.out.println("Current Hour: " + currentHour);
+        // System.out.println(sdFormatter.format(new Date()));
+        // System.out.println(sdFormatter.format(new Date()).getClass().getSimpleName());
+
+        String dayPeriod;
+        if (currentHour > 20){
+            dayPeriod = "night";
+        }
+        else if (currentHour > 17) {
+            dayPeriod = "evening";
+        }
+        else if (currentHour > 12) {
+            dayPeriod = "afternoon";
+        }
+        else {
+            dayPeriod = "morning";
+        }
+
+        String[] possibleGreetings = {
+            String.format("Good %s. Lovely to see you.", dayPeriod),
+            String.format("Good %s. Good to see you.", dayPeriod),
+            String.format("Good %s. How have you been?", dayPeriod)
+        };
+
+        return possibleGreetings[getRandom(possibleGreetings)];
+    }
     
+    // Default version :: name parameter
     public String guestGreeting(String name) {
         // Static array of greetings
         String[] possibleGreetings = {
@@ -19,6 +58,18 @@ public class AlfredQuotes {
         // Return a random greeting from possibleGreetings
         return possibleGreetings[getRandom(possibleGreetings)];
     }
+
+    // Ninja version :: name, dayPeriod parameters
+    public String guestGreeting(String name, String dayPeriod) {
+        String[] possibleGreetings = {
+            String.format("Good %s, %s. Lovely to see you.", dayPeriod, name),
+            String.format("Good %s, %s. Good to see you.", dayPeriod, name),
+            String.format("Good %s, %s. How have you been?", dayPeriod, name)
+        };
+
+        return possibleGreetings[getRandom(possibleGreetings)];
+    }
+
     
     public String dateAnnouncement() {
         Date currentDate = new Date();
@@ -31,8 +82,16 @@ public class AlfredQuotes {
     }
     
     public String respondBeforeAlexis(String conversation) {
-        // YOUR CODE HERE
-        return "for snarky response return string";
+        // Check for "Alexis, Alfred, neither"
+        if (conversation.indexOf("Alexis") > -1) {
+            return "I'm not quite sure why we bother with her at all.";
+        }
+        else if (conversation.indexOf("Alfred") > -1) {
+            return "Of course. Right away.";
+        }
+        else {
+            return "Indeed. Then I shall take my leave.";
+        }
     }
 
     // https://stackoverflow.com/a/8065554/19170401
--- 01_Java_Foundations/Fundamentals/alfredBot/AlfredTest.java
+++ 01_Java_Foundations/Fundamentals/alfredBot/AlfredTest.java
@@ -22,14 +22,30 @@ public class AlfredTest {
             "Maybe that's what Batman is about. Not winning. But failing.."
         );
         
+        // Ninja greetings
+        String ninjagreeting1 = alfredBot.guestGreeting("Bobby Hill", "morning");
+        String ninjagreeting2 = alfredBot.guestGreeting("Brave Sir Robin", "afternoon");
+        String ninjagreeting3 = alfredBot.guestGreeting("Jenny Craig", "evening");
+
+        // Sensei greetings
+        String senseiGreeting1 = alfredBot.guestGreeting();
+        String senseiGreeting2 = alfredBot.guestGreeting();
+        String senseiGreeting3 = alfredBot.guestGreeting();
+
         // Print the greetings to test.
         System.out.println(testGreeting);
         
         // Uncomment these one at a time as you implement each method.
         System.out.println(testGuestGreeting);
         System.out.println(testDateAnnouncement);
-        // System.out.println(alexisTest);
-        // System.out.println(alfredTest);
-        // System.out.println(notRelevantTest);
+        System.out.println(alexisTest);
+        System.out.println(alfredTest);
+        System.out.println(notRelevantTest);
+        System.out.println(ninjagreeting1);
+        System.out.println(ninjagreeting2);
+        System.out.println(ninjagreeting3);
+        System.out.println(senseiGreeting1);
+        System.out.println(senseiGreeting2);
+        System.out.println(senseiGreeting3);
     }
 }
