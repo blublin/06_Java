8e56cdf4c1b14bae6469af276657785ff138a44a
blublin <lublin.ben@gmail.com>
2022-06-05 18:38:42 -0700

    Hashmatique recompiled for Java 8

--- 01_Java_Foundations/Fundamentals/Map_Of_The_Hashmatique/MapMain.java
+++ 01_Java_Foundations/Fundamentals/Map_Of_The_Hashmatique/MapMain.java
@@ -7,248 +7,35 @@ public class MapMain {
 
         trackList.put(
             "Party Party Party",
-            """
-            Well, Party, Party, Party
-            I wanna have a Party
-            I need to have a Party
-            You better have a Party
-            
-            Woah, Party, Party, Party
-            You gotta Party hardy
-            I'm gonna have a Party
-            Or else you will be sorry
-            
-            Oh, Party, Party, Party
-            The Party's gonna start, hey
-            I know a guy named Marty
-            Marty loves to Party
-            Woah, Party, Party, Party
-            Party make me farty
-            I gotta take antacid
-            So I can keep on Partying
-            
-            Sweet, sweet Party
-            Party, Party, Party
-            You gotta Party
-            Hey, I wanna Party
-            
-            Who's gonna make this Party started?
-            Me! Me! ME!
-            
-            Me Me Me Me Me Me Me Me Me Me Me Me
-            Me Me Me Me Me Me Me Me Me Me Me Me
-            """
+            "Well, Party, Party, Party\n" +
+            "I wanna have a Party\n" +
+            "I need to have a Party\n" +
+            "You better have a Party"
         );
-        trackList.put(
+        trackList.putIfAbsent(
             "Friday",
-            """
-            Oo-ooh-ooh, hoo yeah, yeah
-            Yeah, yeah
-            Yeah-ah-ah
-            Yeah-ah-ah
-            Yeah-ah-ah
-            Yeah-ah-ah
-            Yeah, yeah, yeah
-            Seven a.m., waking up in the morning
-            Gotta be fresh, gotta go downstairs
-            Gotta have my bowl, gotta have cereal
-            Seein' everything, the time is goin'
-            Tickin' on and on, everybody's rushin'
-            Gotta get down to the bus stop
-            Gotta catch my bus, I see my friends (My friends)
-            Kickin' in the front seat
-            Sittin' in the back seat
-            Gotta make my mind up
-            Which seat can I take?
-            It's Friday, Friday
-            Gotta get down on Friday
-            Everybody's lookin' forward to the weekend, weekend
-            Friday, Friday
-            Gettin' down on Friday
-            Everybody's lookin' forward to the weekend
-            Partyin', partyin' (Yeah)
-            Partyin', partyin' (Yeah)
-            Fun, fun, fun, fun
-            Lookin' forward to the weekend
-            7: 45, we're drivin' on the highway
-            Cruisin' so fast, I want time to fly
-            Fun, fun, think about fun
-            You know what it is
-            I got this, you got this
-            My friend is by my right, ay
-            I got this, you got this
-            Now you know it
-            Kickin' in the front seat
-            Sittin' in the back seat
-            Gotta make my mind up
-            Which seat can I take?
-            It's Friday, Friday
-            Gotta get down on Friday
-            Everybody's lookin' forward to the weekend, weekend
-            Friday, Friday
-            Gettin' down on Friday
-            Everybody's lookin' forward to the weekend
-            Partyin', partyin' (Yeah)
-            Partyin', partyin' (Yeah)
-            Fun, fun, fun, fun
-            Lookin' forward to the weekend
-            Yesterday was Thursday, Thursday
-            Today i-is Friday, Friday (Partyin')
-            We-we-we so excited
-            We so excited
-            We gonna have a ball today
-            Tomorrow is Saturday
-            And Sunday comes after wards
-            I don't want this weekend to end
-            It's Friday, Friday
-            Gotta get down on Friday
-            Everybody's lookin' forward to the weekend, weekend
-            Friday, Friday
-            Gettin' down on Friday
-            Everybody's lookin' forward to the weekend
-            Partyin', partyin' (Yeah)
-            Partyin', partyin' (Yeah)
-            Fun, fun, fun, fun
-            Lookin' forward to the weekend
-            It's Friday, Friday
-            Gotta get down on Friday
-            Everybody's lookin' forward to the weekend, weekend
-            Friday, Friday
-            Gettin' down on Friday
-            Everybody's lookin' forward to the weekend
-            Partyin', partyin' (Yeah)
-            Partyin', partyin' (Yeah)
-            Fun, fun, fun, fun
-            Lookin' forward to the weekend
-            """
+            "Seven a.m., waking up in the morning\n" +
+            "Gotta be fresh, gotta go downstairs\n" +
+            "Gotta have my bowl, gotta have cereal\n" +
+            "Seein' everything, the time is goin'\n" +
+            "Tickin' on and on, everybody's rushin'\n" +
+            "Gotta get down to the bus stop\n" +
+            "Gotta catch my bus, I see my friends (My friends)"
         );
         trackList.put(
             "Photograph",
-            """
-            Look at this photograph
-            Every time I do, it makes me laugh
-            How did our eyes get so red?
-            And what the hell is on Joey's head?
-            And this is where I grew up
-            I think the present owner fixed it up
-            I never knew we'd ever went without
-            The second floor is hard for sneaking out
-            And this is where I went to school
-            Most of the time had better things to do
-            Criminal record says I broke in twice
-            I must have done it half a dozen times
-            I wonder if it's too late
-            Should I go back and try to graduate?
-            Life's better now than it was back then
-            If I was them, I wouldn't let me in
-            Oh, whoa, whoa
-            Oh, God, I...
-            Every memory of looking out the back door
-            I had the photo album spread out on my bedroom floor
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            Every memory of walking out the front door
-            I found the photo of the friend that I was looking for
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            Goodbye
-            Remember the old arcade
-            Blew every dollar that we ever made
-            The cops hated us hangin' out
-            They say, 'Somebody went and burned it down'
-            We used to listen to the radio
-            And sing along with every song we know
-            We said, 'Someday we'd find out how it feels
-            To sing to more than just the steering wheel'
-            Kim's the first girl I kissed
-            I was so nervous that I nearly missed
-            She's had a couple of kids since then
-            I haven't seen her since God knows when
-            Oh, whoa, whoa
-            Oh, God, I...
-            Every memory of looking out the back door
-            I had the photo album spread out on my bedroom floor
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            Every memory of walking out the front door
-            I found the photo of the friend that I was looking for
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            I miss that town, I miss the faces
-            You can't erase, you can't replace it
-            I miss it now, I can't believe it
-            So hard to stay, too hard to leave it
-            If I could relive those days
-            I know the one thing that would never change
-            Every memory of looking out the back door
-            I had the photo album spread out on my bedroom floor
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            Every memory of walking out the front door
-            I found the photo of the friend that I was looking for
-            It's hard to say it, time to say it
-            Goodbye, goodbye
-            Look at this photograph
-            Every time I do, it makes me laugh
-            Every time I do, it makes me...
-            """
+            "Look at this photograph\n" +
+            "Every time I do, it makes me laugh\n" +
+            "How did our eyes get so red?\n" +
+            "And what the hell is on Joey's head?\n" +
+            "And this is where I grew up"
         );
         trackList.put(
             "All Star",
-            """
-            Somebody once told me the world is gonna roll me
-            I ain't the sharpest tool in the shed
-            She was looking kind of dumb with her finger and her thumb
-            In the shape of an 'L' on her forehead
-            Well the years start coming and they don't stop coming
-            Fed to the rules and I hit the ground running
-            Didn't make sense not to live for fun
-            Your brain gets smart but your head gets dumb
-            So much to do, so much to see
-            So what's wrong with taking the back streets?
-            You'll never know if you don't go
-            You'll never shine if you don't glow
-            Hey now, you're an all-star, get your game on, go play
-            Hey now, you're a rock star, get the show on, get paid
-            And all that glitters is gold
-            Only shooting stars break the mold
-            It's a cool place and they say it gets colder
-            You're bundled up now, wait 'til you get older
-            But the meteor men beg to differ
-            Judging by the hole in the satellite picture
-            The ice we skate is getting pretty thin
-            The water's getting warm so you might as well swim
-            My world's on fire, how about yours?
-            That's the way I like it and I'll never get bored
-            Hey now, you're an all-star, get your game on, go play
-            Hey now, you're a rock star, get the show on, get paid
-            All that glitters is gold
-            Only shooting stars break the mold
-            Hey now, you're an all-star, get your game on, go play
-            Hey now, you're a rock star, get the show, on get paid
-            And all that glitters is gold
-            Only shooting stars
-            Somebody once asked could I spare some change for gas?
-            I need to get myself away from this place
-            I said, 'Yup' what a concept
-            I could use a little fuel myself
-            And we could all use a little change
-            Well, the years start coming and they don't stop coming
-            Fed to the rules and I hit the ground running
-            Didn't make sense not to live for fun
-            Your brain gets smart but your head gets dumb
-            So much to do, so much to see
-            So what's wrong with taking the back streets?
-            You'll never know if you don't go (go!)
-            You'll never shine if you don't glow
-            Hey now, you're an all-star, get your game on, go play
-            Hey now, you're a rock star, get the show on, get paid
-            And all that glitters is gold
-            Only shooting stars break the mold
-            And all that glitters is gold
-            Only shooting stars break the mold
-            """
-
+            "Somebody once told me the world is gonna roll me\n" +
+            "I ain't the sharpest tool in the shed\n" +
+            "She was looking kind of dumb with her finger and her thumb\n" +
+            "In the shape of an 'L' on her forehead\n"
         );
             
         String partyLyrics = trackList.get("Party Party Party");
@@ -257,7 +44,7 @@ public class MapMain {
         Set<String> trackTitles = trackList.keySet();
         for (String track : trackTitles) {
             System.out.println("Song Title: " + track);
-            System.out.printf("Some Lyrics: %80s\n\n", trackList.get(track));
+            System.out.printf("Some Lyrics:\n%80s\n\n", trackList.get(track));
         }
     }
 }
